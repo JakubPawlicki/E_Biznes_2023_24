14c57e3f0b1db5c51c6881addf6752d555df0dc6
JakubPawlicki <100231154+JakubPawlicki@users.noreply.github.com>
2024-07-18 21:02:41 +0200

    Update Product.jsx

--- zad_05_frontend/frontend/src/components/Product/Product.jsx
+++ zad_05_frontend/frontend/src/components/Product/Product.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Product.css';
-import {Link, useLocation} from "react-router-dom";
-import image from "../Assets/default_product_image.png"
+import { Link, useLocation } from "react-router-dom";
+import image from "../Assets/default_product_image.png";
 
 export const Product = (props) => {
-
     const location = useLocation();
+
     return (
         <Link to={`${location.pathname}/${props.ID}`}>
             <div className="product">
-                <img src={props.image == null ? image : props.image} alt=""/>
+                <img src={props.image == null ? image : props.image} alt={props.Name} />
                 <p>{props.Name}</p>
                 <p className="product-price">{props.Price}</p>
             </div>
         </Link>
-    )
-        ;
+    );
+};
+
+Product.propTypes = {
+    ID: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    Name: PropTypes.string.isRequired,
+    Price: PropTypes.string.isRequired
 };
