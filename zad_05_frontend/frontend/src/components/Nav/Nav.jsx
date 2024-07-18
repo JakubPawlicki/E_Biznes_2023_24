ec9172934fadf13b86a48ad13a5ebf6a4cdf9125
JakubPawlicki <100231154+JakubPawlicki@users.noreply.github.com>
2024-07-18 20:52:37 +0200

    Update Nav.jsx

--- zad_05_frontend/frontend/src/components/Nav/Nav.jsx
+++ zad_05_frontend/frontend/src/components/Nav/Nav.jsx
@@ -1,34 +1,28 @@
-import { FiShoppingCart } from "react-icons/fi";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { useShoppingCart } from "../../context/ShoppingCartContentProvider";
-import "./Nav.css";
+import {FiShoppingCart} from "react-icons/fi";
+import {useState} from "react";
+import {Link} from "react-router-dom";
+import {useShoppingCart} from "../../context/ShoppingCartContentProvider";
+import "./Nav.css"
 
 export function Nav() {
-    const { cartQuantity } = useShoppingCart();
-    const [menu, setMenu] = useState("");
 
-    const handleCartKeyDown = (event) => {
-        if (event.key === 'Enter' || event.key === ' ') {}
-    };
+    const {cartQuantity} = useShoppingCart()
+
+    const [menu, setMenu] = useState("")
 
     return (
         <nav>
             <div className="nav-logo"></div>
             <ul className="nav-menu">
-                <li onClick={() => setMenu("laptops")} className={menu === "laptops" ? "chosen" : ""}>
-                    <Link to="/laptopy">Laptopy</Link>
-                </li>
-                <li onClick={() => setMenu("smartphones")} className={menu === "smartphones" ? "chosen" : ""}>
-                    <Link to="/smartfony">Smartfony</Link>
-                </li>
-                <li onClick={() => setMenu("headphones")} className={menu === "headphones" ? "chosen" : ""}>
-                    <Link to="/sluchawki">Słuchawki</Link>
-                </li>
+                <li onClick={() => setMenu("laptops")} className={menu === "laptops" ? "chosen" : ""}><Link
+                    to="/laptopy">Laptopy</Link></li>
+                <li onClick={() => setMenu("smartphones")} className={menu === "smartphones" ? "chosen" : ""}><Link
+                    to="/smartfony">Smartfony</Link></li>
+                <li onClick={() => setMenu("headphones")} className={menu === "headphones" ? "chosen" : ""}><Link
+                    to="/sluchawki">Słuchawki</Link></li>
             </ul>
-            <div className="nav-cart" tabIndex={0} onClick={() => {}}>
-                <Link to="/koszyk">
-                    <FiShoppingCart size={25} />
+            <div className="nav-cart">
+                <Link to="/koszyk"><FiShoppingCart size={25}/>
                     <div className="nav-cart-count">{cartQuantity}</div>
                 </Link>
             </div>
