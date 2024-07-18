9a7933db1dcf24b2584df6de0a60c8e6bd5402de
JakubPawlicki <100231154+JakubPawlicki@users.noreply.github.com>
2024-07-18 20:39:01 +0200

    Update Category.jsx

--- zad_05_frontend/frontend/src/pages/Category.jsx
+++ zad_05_frontend/frontend/src/pages/Category.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import {ProductList} from "../components/ProductList/ProductList";
+import PropTypes from 'prop-types';
+import { ProductList } from "../components/ProductList/ProductList";
 
 export function Category(props) {
     return (
-        <div><ProductList category={props.category}>
-        </ProductList></div>
+        <div>
+            <ProductList category={props.category} />
+        </div>
     );
 }
+
+Category.propTypes = {
+    category: PropTypes.string.isRequired,
+};
