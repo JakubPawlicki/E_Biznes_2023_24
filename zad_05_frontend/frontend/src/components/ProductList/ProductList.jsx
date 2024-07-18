9ef035d7f1b931dd6643fea27294fd1c37575544
JakubPawlicki <100231154+JakubPawlicki@users.noreply.github.com>
2024-07-18 20:59:57 +0200

    Update ProductList.jsx

--- zad_05_frontend/frontend/src/components/ProductList/ProductList.jsx
+++ zad_05_frontend/frontend/src/components/ProductList/ProductList.jsx
@@ -1,34 +1,39 @@
-import React, {useEffect, useState} from 'react';
-import "./ProductList.css"
-import {Product} from "../Product/Product";
-import {axiosInstance} from "../../api/api";
-export const ProductList = (props) => {
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types'; // Import PropTypes
+import "./ProductList.css";
+import { Product } from "../Product/Product";
+import { axiosInstance } from "../../api/api";
 
-    const [productList, setProductList] = useState([])
+export const ProductList = (props) => {
+    const [productList, setProductList] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
             try {
-                const products = await axiosInstance.get(`/categories/${props.category}`)
-                setProductList(products.data.Products)
+                const products = await axiosInstance.get(`/categories/${props.category}`);
+                setProductList(products.data.Products);
             } catch (err) {
-                console.log(err.response.data)
-                console.log(err.response.status)
-                console.log(err.response.headers)
+                console.log(err.response.data);
+                console.log(err.response.status);
+                console.log(err.response.headers);
             }
         }
-        fetchData()
+        fetchData();
     }, [props.category]);
 
     return (
         <div className="offers-section">
             <h1>Produkty</h1>
-            <hr/>
+            <hr />
             <div className="offers-list">
-                {productList.map((item, index) => {
-                    return <Product key={index} {...item}></Product>
-                })}
+                {productList.map((item) => (
+                    <Product key={item.id} {...item} />
+                ))}
             </div>
         </div>
     );
+};
+
+ProductList.propTypes = {
+    category: PropTypes.string.isRequired,
 };
